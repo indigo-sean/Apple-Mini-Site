a5a7e2960ee42864fde6f0ab40acaaeac8b96a43
Matt Michaels <matt.michaels@indigoslate.com>
2016-06-13 14:56:36 -0700

    ready for rough client review

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
+
+// Toggling for the SKU Chooser
+$(document).on("click", ".products-windows", function() {
+  $('.products-windows').removeClass('active-windows');
+  $(this).addClass('active-windows');
+});
+
+$(document).on("click", ".products-mac", function() {
+  $('.products-mac').removeClass('active-mac');
+  $(this).addClass('active-mac');
+});
+
+
+
 // Demo Material ///////////////////////////////////////////////////////////////////////
 
   // Creating array out of qquestions
-  $( "#array-form" ).submit(function( event ) {
+  $( "#contact-form" ).submit(function( event ) {
+    event.preventDefault();
     
     // Turn form into array
-    var x = $("#array-form").serializeArray();
-    event.preventDefault();
-
-
+    var x = $("#question-form").serializeArray();
+    
     // Begin Logging ///////////////////////////
       console.log(x);
       alert(JSON.stringify(x));
+
       $.each(x, function(i, field){
         $("#results").append("<p>" + field.name + ":" + field.value + "</p>");
       });
@@ -21,16 +35,44 @@
 
 
 
+// Counting the questions
+var totalQuestions = $('.quiz__question').size();
+var currentQuestion = 0;
 
-$(document).on("click", ".products-windows", function() {
-  $('.products-windows').removeClass('active-windows');
-  console.log('sup')
-  $(this).addClass('active-windows');
+
+$questions = $('.quiz__question');
+$questions.hide();
+$($questions.get(currentQuestion)).show();
+
+$(".question__submit--container button").css("opacity", ".2").prop("disabled", true);
+
+$('.quiz__question input').click( function() {
+  $(".question__submit--container button").css("opacity", "1").prop("disabled", false);
 });
 
-$(document).on("click", ".products-mac", function() {
-  $('.products-mac').removeClass('active-mac');
-  console.log('sup mac')
-  $(this).addClass('active-mac');
+
+
+$('.question__submit').click(function () {
+  event.preventDefault();
+  $(this).parent(".question__submit--container").hide();
+  $(".question__submit--container button").css("opacity", ".2").prop("disabled", true);
+
+  // Add +1 per question
+  $($questions.get(currentQuestion)).show(function () {
+    currentQuestion = currentQuestion + 1;
+
+    // Fall back if needed
+    if (currentQuestion == totalQuestions) {
+      console.log('completed');
+    } else {
+      // Show the next.
+      $($questions.get(currentQuestion)).slideDown();
+    }
+  });
 });
 
+
+// Toggling rules section
+$('.rules__expander').click( function() {
+  $('.rules__content').slideToggle();
+});
