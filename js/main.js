54f1ef49fa427351cd9775d9655fbf992d4641bb
Matt Michaels <matt.michaels@indigoslate.com>
2016-06-14 10:02:47 -0700

    re-styling, notes added to main.js for JSON string request from client

--- js/main.js
+++ js/main.js
@@ -12,7 +12,14 @@ $(document).on("click", ".products-mac", function() {
 
 
 
-// Demo Material ///////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+// Question Capture //////////////////////////////////////////////////////////////////
 
   // Creating array out of qquestions
   $( "#contact-form" ).submit(function( event ) {
@@ -25,6 +32,7 @@ $(document).on("click", ".products-mac", function() {
       console.log(x);
       alert(JSON.stringify(x));
 
+      // Example dump to #results panel
       $.each(x, function(i, field){
         $("#results").append("<p>" + field.name + ":" + field.value + "</p>");
       });
@@ -34,42 +42,62 @@ $(document).on("click", ".products-mac", function() {
 ///////////////////////////////////////////////////////////////////////////////////////
 
 
+// Goals ///////////////
+// 1. Capture 3 answers from the '#question-form' <form>
+//    a. Show next '.quiz__question' after .click() on '.question__submit'
+// 2. Include a unique GUID with the JSON string of quiz answers
+// 3. Capture contact info from the #contact-form <form>
+// 4. Submit everything together (dump to #results for demo)
+//  
+// Note: Send one JSON string to the REST API containing quiz answers, GUID, and form fields.
+////////////////////////
+
+
+
+
+
+
+
+
+
+
+
 
 // Counting the questions
-var totalQuestions = $('.quiz__question').size();
-var currentQuestion = 0;
+// var totalQuestions = $('.quiz__question').size();
+// var currentQuestion = 0;
 
 
-$questions = $('.quiz__question');
-$questions.hide();
-$($questions.get(currentQuestion)).show();
+// $questions = $('.quiz__question');
+// $questions.hide();
+// $($questions.get(currentQuestion)).show();
 
-$(".question__submit--container button").css("opacity", ".2").prop("disabled", true);
+// $(".question__submit--container button").css("opacity", ".2").prop("disabled", true);
 
-$('.quiz__question input').click( function() {
-  $(".question__submit--container button").css("opacity", "1").prop("disabled", false);
-});
+// $('.quiz__question input').click( function() {
+//   $(".question__submit--container button").css("opacity", "1").prop("disabled", false);
+// });
 
 
 
-$('.question__submit').click(function () {
-  event.preventDefault();
-  $(this).parent(".question__submit--container").hide();
-  $(".question__submit--container button").css("opacity", ".2").prop("disabled", true);
+// $('.question__submit').click(function () {
+//   event.preventDefault();
+//   $(this).parent(".question__submit--container").hide();
+//   $(".question__submit--container button").css("opacity", ".2").prop("disabled", true);
 
-  // Add +1 per question
-  $($questions.get(currentQuestion)).show(function () {
-    currentQuestion = currentQuestion + 1;
+//   // Add +1 per question
+//   $($questions.get(currentQuestion)).show(function () {
+//     currentQuestion = currentQuestion + 1;
 
-    // Fall back if needed
-    if (currentQuestion == totalQuestions) {
-      console.log('completed');
-    } else {
-      // Show the next.
-      $($questions.get(currentQuestion)).slideDown();
-    }
-  });
-});
+//     // Fall back if needed
+//     if (currentQuestion == totalQuestions) {
+//       console.log('completed');
+//     } else {
+//       // Show the next.
+//       $($questions.get(currentQuestion)).slideDown();
+//     }
+//   });
+// });
 
 
 // Toggling rules section
